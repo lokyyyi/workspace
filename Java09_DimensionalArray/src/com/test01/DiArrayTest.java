b61f27f8390ecc51177d75ee3a77f09f2a28289f
lokyyyi <jyw9011@naver.com>
2023-09-19 16:41:24 +0900

    no message

---
+++ Java09_DimensionalArray/src/com/test01/DiArrayTest.java
@@ -0,0 +1,18 @@
+package com.test01;
+
+public class DiArrayTest {
+
+	public static void main(String[] args) {
+		
+	}
+	
+	public void printArr(int[][] iarr) {
+		for(int i=0; i<iarr.length; i++) {
+			for(int j=0; j<iarr[i].length; j++) {
+				System.out.println("\t " + iarr[i][j]);
+			}
+		}
+	}
+}
+
+
